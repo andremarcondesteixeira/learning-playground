fc650381abd901c1d0e5156cf5c6fa1585013f46
André Teixeira <andremarcondesteixeira@gmail.com>
2025-01-25 22:33:53 -0300

    propagation networks

--- Propagation Networks/propagation-network.ts
+++ Propagation Networks/propagation-network.ts
@@ -5,28 +5,60 @@ Once a propagator is run, by default it will not run again unless it gets queued
 Every propagator that is queued will eventually run.
 */
 
-let f = makeCell();
-let c = makeCell();
-fahrenheitToCelsius(f, c);
-addContent(f, 77); // 77 will never be forgotten by the cell
-content(c);
+/**
+ * A Cell does not have any removeContent or alterContent methods. That is on purpose.
+ * Cells hold state and a list of every propagator watching them.
+ * When the cell’s state changes, it notifies all the propagators.
+ */
+class Cell {
+    #content: any[] = [undefined];
+    #neighbors: unknown[] = [];
 
-function fahrenheitToCelsius(f, c) {
-    let thirtyTwo = makeCell();
-    let f32 = makeCell();
-    let five = makeCell();
-    let cTimes9 = makeCell();
-    let nine = makeCell();
+    /**
+     * Extracts the cell's current content
+     */
+    get content() {
+        return this.#content[this.#content.length - 1];
+    }
+
+    get neighbors() {
+        return [...this.#neighbors];
+    }
+
+    /**
+     * Adds some content to the cell
+     */
+    addContent(value: any) {
+        this.#content.push(value);
+    }
+
+    /**
+     * Asserts that a propagator should be queued when the cell's content changes
+     */
+    newNeighbor(neighbor: unknown) {
+        this.#neighbors.push(neighbor);
+    }
+}
+
+let fahrenheitCell = new Cell();
+let celsiusCell = new Cell();
+fahrenheitToCelsius(fahrenheitCell, celsiusCell);
+fahrenheitCell.addContent(77); // 77 will never be forgotten by the cell
+const content = celsiusCell.content();
+console.log(content);
+
+function fahrenheitToCelsius(fahrenheitCell: Cell, celsiusCell: Cell) {
+    let thirtyTwo = new Cell();
+    let f32 = new Cell();
+    let five = new Cell();
+    let cTimes9 = new Cell();
+    let nine = new Cell();
     constant(thirtyTwo, 32);
     constant(five, 5);
     constant(nine, 9);
-    subtractor(f, thirtyTwo, f32);
+    subtractor(fahrenheitCell, thirtyTwo, f32);
     multiplier(f32, five, cTimes9);
-    divider(cTimes9, nine, c);
-}
-
-function makeCell(): Cell {
-    return new Cell();
+    divider(cTimes9, nine, celsiusCell);
 }
 
 function constant(cell: Cell, value: any) {
@@ -43,39 +75,4 @@ function multiplier(a, b, c) {
 
 function divider(a, b, c) {
     // TODO
-}
-
-/**
- * A Cell does not have any removeContent or alterContent methods. That is on purpose.
- * Cells hold state and a list of every propagator watching them.
- * When the cell’s state changes, it notifies all the propagators.
- */
-class Cell {
-    #content: any;
-
-    constructor() {
-        // TODO
-    }
-
-    /**
-     * Extracts the cell's current content
-     */
-    get content() {
-        return this.#content;
-    }
-
-    /**
-     * Adds some content to the cell
-     */
-    addContent(value: any) {
-        // TODO
-    }
-
-    /**
-     * Asserts that a propagator should be queued when the cell's content changes
-     */
-    newNeighbor(neighbor: unknown) {
-        // TODO
-    }
-
 }
